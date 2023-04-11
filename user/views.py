26a0afd99908f008fbabfe85b607a7c48cf3a3b9
I Joo Han <juhanlee2218@gmail.com>
2023-04-11 13:12:05 +0900

    second commit - 이메일 DB저장 오류 수정& 이메일 중복으로 가입되는 오류 수정 & CSS수정

--- user/views.py
+++ user/views.py
@@ -16,7 +16,7 @@ def sign_up_view(request):
         username = request.POST.get('username', '')
         password = request.POST.get('password', '')
         password2 = request.POST.get('password2', '')
-        bio = request.POST.get('bio', '')
+        email = request.POST.get('email', '')
 
         if password != password2:
             return render(request, 'user/signup.html', {'error': '패스워드를 확인 해 주세요!'})
@@ -24,11 +24,18 @@ def sign_up_view(request):
             if username == '' or password == '':
                 return render(request, 'user/signup.html', {'error': '사용자 이름과 패스워드는 필수 값 입니다'})
 
-            exist_user = get_user_model().objects.filter(username=username)
-            if exist_user:
+            exist_user_name = get_user_model().objects.filter(username=username)
+            exist_user_email = get_user_model().objects.filter(email=email)
+            # exist_user_password = get_user_model().objects.filter(password=password)
+            if exist_user_name:
                 return render(request, 'user/signup.html', {'error':'사용자가 존재합니다.'})
+            elif exist_user_email:
+                return render(request, 'user/signup.html', {'error':'사용자가 존재합니다.'})
+            # elif exist_user_password:
+            #     return render(request, "user/signup.html", {'error':'사용중인 비밀번호 입니다.'})
             else:
-                UserInfo.objects.create_user(username=username, password=password, bio=bio)
+                print(exist_user_name)
+                UserInfo.objects.create_user(username=username, password=password, email=email)
                 return redirect('/sign-in')
             
             
