e3f24427529a343a65df24acbf5f675b9bc73a9b
Yang ye lin <oky07031217@gmail.com>
2023-04-12 00:28:56 +0900

    게시물등록

--- posting/views.py
+++ posting/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import UserInfo
-from django.contrib.auth import get_user_model
-from django.contrib import auth
+from .models import PostingModel,UserInfo
 from django.contrib.auth.decorators import login_required
 
 
@@ -10,11 +8,43 @@ def home_view(request):
 
         return render(request, 'posting/home.html')
 
-def posting_view(request): # 트윗 안에 홈.에이치티엠엘을 보여주는 함수
+def posting_view(request):
     if request.method == 'GET':
 
         return render(request, 'posting/posting.html')
 
     elif request.method =='POST':
+        user = request.user
 
-        return ''
+        author = user
+        title = request.POST.get('title','')
+        thumbnail = request.POST.get('thumbnail','')
+        content = request.POST.get('content','')
+
+        if title == '':
+            return render(request, 'posting/posting.html', {'error': '제목을 작성해주세요!'})
+        elif content == '':
+            return render(request, 'posting/posting.html', {'error': '내용을 작성해주세요!'})
+        else:
+            PostingModel.objects.create(author=author,title=title,thumbnail=thumbnail, content=content)
+            return redirect('/api/posting-detail/<int:id>')
+
+
+
+def posting_detail_view(request,id):
+    if request.method == 'GET':
+        select_posting = PostingModel.objects.get(id=id)
+        return render(request, 'posting/posting_detail.html',{'select_posting':select_posting})
+
+    elif request.method == 'POST':
+
+        return redirect('/api/posting-detail/<int:id>')
+
+def mypage_view(request):
+    if request.method == 'GET':
+
+        # return redirect('/api/mypage/<str:username>')
+
+        return render(request, 'posting/mypage.html')
+
+
